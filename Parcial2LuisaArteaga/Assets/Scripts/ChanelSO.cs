21fa7bbb437029cfd5d46ab6b53dbef20525f1e7
Luisa Arteaga <luisa.arteaga@upb.edu.co>
2021-10-09 19:08:20 -0500

    Script v1
    y materiales

---
+++ Parcial2LuisaArteaga/Assets/Scripts/ChanelSO.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class ChanelSO : ScriptableObject
+{
+    //patron observer
+    public delegate void Cocinado();
+    public event Cocinado cocinado;
+
+    public delegate void ListoHuevo();
+    public event ListoHuevo listoHuevo;
+
+    public void Cocinando_()
+    {
+        cocinado?.Invoke();
+    }
+
+    public void Listo_Huevo()
+    {
+        listoHuevo?.Invoke();
+    }
+}
---
+++ Parcial2LuisaArteaga/Assets/Scripts/ColorController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorController : MonoBehaviour
+{
+    [SerializeField] ChanelSO channelEvents;
+    [SerializeField] Slider slider;
+
+    [SerializeField] Renderer huevoColor;
+    //ColorCreator colorGenerator = new ColorCreator();
+
+    public Text textico;
+    //StateMachine stateMachine = new StateMachine();
+    //States estados = new EstadoListo();
+
+    public void Awake()
+    {
+
+    }
+
+    public void OnEnable()
+    {
+        channelEvents.cocinado += CambiaColorHuevo;
+    }
+    public void OnDisable()
+    {
+        channelEvents.cocinado -= CambiaColorHuevo;
+    }
+
+    public void CambiaColorHuevo()
+    {
+        if (slider.value < 1 / 4) stateMachine.HuevoCrudo(textico);
+
+        if (slider.value >= 1 / 2)
+        {
+            colorGenerator.huevoCrudo(huevoColor);
+            stateMachine.huevoCocinando(textico);
+        }
+        if (slider.value > 0.8f)
+        {
+            colorGenerator.HuevoListo(huevoColor);
+            stateMachine.HuevoListo(textico);
+        }
+    }
+}
